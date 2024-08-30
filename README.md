04ef879c7671924e4fb7ef19c23d0e01c5eafaed
JE3KMZ <je3kmz@FreeBSD.org>
2024-08-30 01:50:59 +0000

    jcba 用と fmplapla 用ストリーム取得スクリプト
    Initial commit
    
    Changes to be committed:
    	new file:   README.md
    	new file:   rec_wss.py

---
+++ README.md
@@ -0,0 +1,3 @@
+# JCBA 及び FM++ のストリーミング受信支援スクリプト
+元ネタ
+https://kemasoft.net/?Stories/2022-04-04
---
+++ rec_wss.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+# coding: utf-8
+# vim:fenc=utf-8 ff=unix ft=python ts=4 sw=4 sts=4 si et :
+
+"""
+pre-flight:
+    pip install requests websocket-client
+
+usage:
+    rec_wss --provider jcba --station heartfm --time 1800 | ffplay -nodisp -hide_banner -autoexit -i pipe:
+    rec_wss --provider fmplapla --station amasakifm | mpv -
+    env DEBUG=1 rec_wss -p jcba -s fmkaratsu # debug print
+"""
+
+import os
+import sys
+import time
+import argparse
+import requests
+import websocket
+import pprint
+import http
+
+
+class wss:
+    def __init__(self, provider, station_id, duration=0):
+        self.station_id = station_id
+        self.duration = duration
+        self.start_time = time.time()
+        if 'DEBUG' in os.environ:
+            websocket.enableTrace(True)
+            http.client.HTTPConnection.debuglevel=1
+
+        # token, location
+        if provider == 'jcba':
+            headers = {
+                    "Origin": "https://www.jcbasimul.com"
+                    }
+            url = 'https://api.radimo.smen.biz/api/v1/select_stream?station={st}&channel=0&quality=high&burst=5'.format(st=self.station_id)
+        elif provider == 'fmplapla':
+            headers = {
+                    "Origin": "https://fmplapla.com"
+                    }
+            url = 'https://fmplapla.com/api/select_stream?station={st}&burst=5'.format(st=self.station_id)
+        else:
+            print('Not provider.')
+            print('jcba or fmplapla.')
+            sys.exit()
+        res = requests.post(url, headers=headers)
+        json = res.json()
+        if 'DEBUG' in os.environ:
+            print(url)
+            pprint.pprint(json)
+        self.token = json['token']
+        self.location = json['location']
+
+        self.ws = websocket.WebSocketApp(
+            self.location,
+            subprotocols=['listener.fmplapla.com'],
+            on_open=self._on_open, on_message=self._on_message)
+        try:
+            self.ws.run_forever()
+        except (Exception, KeyboardInterrupt, SystemExit) as e:
+            self.ws.close()
+
+    def _on_message(self, data, message):
+        if data:
+            sys.stdout.buffer.write(message)
+        if self.duration > 0:
+            if self.duration < (time.time() - self.start_time):
+                raise KeyboardInterrupt
+
+    def _on_open(self, data):
+        self.ws.send(self.token)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='example: python rec_wss.py -p jcba -s fmkaratsu -t 1800 | ffplay -i pipe:')
+    parser.add_argument('-p', '--provider', required=True,
+                        help='provider. jcba or fmplapla')
+    parser.add_argument('-s', '--station', required=True,
+                        help='station id. example: fmkaratsu')
+    parser.add_argument('-t', '--time', type=int, default=0,
+                        help='stop writing the output after its seconds reaches duration. it defaults to 0, meaning that loop forever.')
+    args = parser.parse_args()
+    radio = wss(args.provider, args.station, args.time)
+
+
+if __name__ == '__main__':
+    main()
